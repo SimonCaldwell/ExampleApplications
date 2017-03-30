46debf9d1d74aa98a3db9f0b47d4b4b0ce6f9d6c
DavidBetteridge <davidabetteridge@gmail.com>
2017-03-30 19:13:48 +0100

    Nominal Validation

---
+++ P2P/Nominal Validation/PROACTIS.ExampleApplications.ExampleNominalCheck/ObjectModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace PROACTIS.ExampleApplications.ExampleNominalCheck
+{
+    internal class ObjectModel
+    {
+        private const string NS = "http://www.getrealsystems.com/xml/xml-ns";
+        internal static Details FromXml(string nominalsXML)
+        {
+
+            // Load the xml
+            var nt = new NameTable();
+            var nsmgr = new XmlNamespaceManager(nt);
+            nsmgr.AddNamespace("grs", NS);
+
+            var dom = new XmlDocument(nt);
+            dom.LoadXml(nominalsXML);
+
+            // Populate an object model
+            var details = new Details();
+
+            var database = (XmlElement)dom.DocumentElement.SelectSingleNode("grs:Database", nsmgr);
+            if (database == null) throw new Exception("XML does not contain an element called database.");
+            details.DatabaseServer = database.GetAttribute("grs:Server", NS);
+            details.Database = database.GetAttribute("grs:DatabaseName", NS);
+
+            var general = (XmlElement)dom.DocumentElement.SelectSingleNode("grs:General", nsmgr);
+            if (general == null) throw new Exception("XML does not contain an element called general.");
+            details.UserGUID = general.GetAttribute("grs:UserGUID", NS);
+            details.CompanyGUID = general.GetAttribute("grs:CompanyGUID", NS);
+
+            details.Nominals = new List<Nominal>();
+            foreach (var nom in dom.DocumentElement.SelectNodes("grs:Nominals/grs:Nominal", nsmgr))
+            {
+                var nominal = new Nominal();
+                details.Nominals.Add(nominal);
+
+                nominal.Coding = general.GetAttribute("grs:Coding", NS);
+                nominal.Element1 = general.GetAttribute("grs:Element1", NS);
+                nominal.Element2 = general.GetAttribute("grs:Element2", NS);
+                nominal.Element3 = general.GetAttribute("grs:Element3", NS);
+                nominal.Element4 = general.GetAttribute("grs:Element4", NS);
+                nominal.Element5 = general.GetAttribute("grs:Element5", NS);
+                nominal.Element6 = general.GetAttribute("grs:Element6", NS);
+                nominal.Element7 = general.GetAttribute("grs:Element7", NS);
+                nominal.Element8 = general.GetAttribute("grs:Element8", NS);
+            }
+
+            return details;
+        }
+
+        internal static string ToXml(Details details)
+        {
+            // Create a new DOM
+            var nt = new NameTable();
+            var nsmgr = new XmlNamespaceManager(nt);
+            nsmgr.AddNamespace("grs", NS);
+
+            // Add any failed nominals to the xml
+            var dom = new XmlDocument(nt);
+            dom.AppendChild(dom.CreateElement("grs:Nominals", NS));
+
+            foreach (var nom in details.Nominals.Where(n => !n.IsValid))
+            {
+                var nominal = dom.CreateElement("grs:Nominal");
+                dom.DocumentElement.AppendChild(nominal);
+
+                nominal.SetAttribute("Coding", NS, nom.Coding);
+                nominal.SetAttribute("Element1", NS, nom.Element1);
+                nominal.SetAttribute("Element2", NS, nom.Element2);
+                nominal.SetAttribute("Element3", NS, nom.Element3);
+                nominal.SetAttribute("Element4", NS, nom.Element4);
+                nominal.SetAttribute("Element5", NS, nom.Element5);
+                nominal.SetAttribute("Element6", NS, nom.Element6);
+                nominal.SetAttribute("Element7", NS, nom.Element7);
+                nominal.SetAttribute("Element8", NS, nom.Element8);
+                nominal.SetAttribute("ValidNominal", NS, "False");
+            }
+
+            return dom.OuterXml;
+        }
+    }
+
+    internal class Nominal
+    {
+        public string Coding { get; internal set; }
+        public string Element1 { get; internal set; }
+        public string Element2 { get; internal set; }
+        public string Element3 { get; internal set; }
+        public string Element4 { get; internal set; }
+        public string Element5 { get; internal set; }
+        public string Element6 { get; internal set; }
+        public string Element7 { get; internal set; }
+        public string Element8 { get; internal set; }
+        public bool IsValid { get; set; }
+        public Nominal()
+        {
+            this.IsValid = true;
+        }
+    }
+
+    internal class Details
+    {
+        public string DatabaseServer { get; internal set; }
+        public string Database { get; internal set; }
+        public string UserGUID { get; internal set; }
+        public string CompanyGUID { get; internal set; }
+        public List<Nominal> Nominals { get; internal set; }
+    }
+}
---
+++ P2P/Nominal Validation/PROACTIS.ExampleApplications.ExampleNominalCheck/Services.cs
@@ -0,0 +1,36 @@
+﻿namespace PROACTIS.ExampleApplications.ExampleNominalCheck
+{
+    public class Services : P2P.grsCustInterfaces.INominals
+    {
+        public bool NominalCheck(string NominalsXML, string POXML, ref string ErrorNominals)
+        {
+            // Convert the supplied nominals xml into a object model
+            var details = ObjectModel.FromXml(NominalsXML);
+
+            // Check each nominal in turn
+            var allValid = true;  //Assume the best
+            foreach (var nominal in details.Nominals)
+            {
+                /*******************************
+                 * 
+                 *     Place your code here
+                 * 
+                 ******************************/
+
+                // If this nominal is not valid then...
+                allValid = false;
+                nominal.IsValid = false;
+            }
+
+            if (!allValid)
+            {
+                // At least one nominal has failed validation,  so we need to return the failures
+                ErrorNominals = ObjectModel.ToXml(details);
+            }
+
+            return allValid;
+
+
+        }
+    }
+}

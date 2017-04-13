e7eabcb9f9351f86fdeb5a175d482a6bed562eb2
DavidBetteridge <davidabetteridge@gmail.com>
2017-04-13 12:08:53 +0100

    Document Export

---
+++ P2P/Exports/PROACTIS.ExampleApplication.SimpleExportProcessor/Services.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PROACTIS.P2P.grsCustInterfaces;
+using System.IO;
+
+namespace PROACTIS.ExampleApplication.SimpleExportProcessor
+{
+    public class Services : IExportProcessor
+    {
+        ExportProcessorInitialiseResult IExportProcessor.Initialise(int numberOfDocuments, string databaseTitle, string databaseName, string databaseServerName)
+        {
+            // Each document will be written to it's own file,  so each document can be in it's own transaction.
+            return ExportProcessorInitialiseResult.OneTransactionPerDocument;
+        }
+
+        ExportProcessorExportResult IExportProcessor.ProcessDocument(Guid guid, string documentNumber, string documentXml, Guid documentGuid, string documentType, string description)
+        {
+            var filename = Path.Combine(@"c:\temp", documentNumber + ".xml");
+            File.WriteAllText(filename, documentXml);
+
+            return ExportProcessorExportResult.Success;
+        }
+
+        void IExportProcessor.PostingComplete()
+        {
+            // Nothing to do
+        }
+    }
+}
